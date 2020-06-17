be9940a3028aee93b9cade6e5c346cea3a993241
David Liu <chihweiliu1993@gmail.com>
2020-06-17 22:42:21 +0100

    improve link logic

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -8,7 +8,39 @@ const CustomNav = () => {
   const router = useRouter();
   const { currentUser, signout } = useAuthContext();
 
-  console.log("currentUser nav", currentUser);
+  const renderLink = (link: { label: string; callback: () => void }) => {
+    return (
+      <Nav.Link onClick={link.callback} className="text-muted">
+        {link.label}
+      </Nav.Link>
+    );
+  };
+
+  const renderLinks = () => {
+    const links = {
+      signOut: { label: "Sign Out", callback: signout },
+      signIn: { label: "Sign In", callback: () => router.push("/signin") },
+      signUp: { label: "Sign Up", callback: () => router.push("/signup") },
+    };
+
+    if (currentUser) {
+      return renderLink(links.signOut);
+    }
+
+    switch (router.pathname) {
+      case "/signin":
+        return renderLink(links.signUp);
+      case "/signup":
+        return renderLink(links.signIn);
+      default:
+        return (
+          <>
+            {renderLink(links.signIn)}
+            {renderLink(links.signUp)}
+          </>
+        );
+    }
+  };
 
   return (
     <Navbar bg="light" expand="lg">
@@ -21,29 +53,10 @@ const CustomNav = () => {
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        {currentUser ? (
-          <>
-            <Nav.Item className="text-dark">{currentUser.email}</Nav.Item>
-            <Nav.Link onClick={signout} className="text-muted">
-              Sign Out
-            </Nav.Link>
-          </>
-        ) : (
-          <>
-            <Nav.Link
-              onClick={() => router.push("/signin")}
-              className="text-muted"
-            >
-              Sign In
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => router.push("/signup")}
-              className="text-muted"
-            >
-              Sign Up
-            </Nav.Link>
-          </>
+        {currentUser && (
+          <Nav.Item className="text-dark">{currentUser.email}</Nav.Item>
         )}
+        {renderLinks()}
       </Navbar.Collapse>
     </Navbar>
   );
